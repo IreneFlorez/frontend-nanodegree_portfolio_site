2332f10e657f1fb2e3346dc6568c12f8db327672
Irene Florez <gummyrene@gmail.com>
2017-07-29 12:32:21 -0700

    July 29 draft progress review

---
+++ js/script.js
@@ -0,0 +1,17 @@
+  <!-- Modal -->
+<div class = "modal fade" id="project1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel"> Project 1</h4>
+          </div>
+          <div class="modal-body">
+            <img class="img-responsive" src="link">
+            Text
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+              </div>
+              </div>
+              </div>
